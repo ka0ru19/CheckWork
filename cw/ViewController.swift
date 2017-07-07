322f1770d5d0354e702ca3b1fd3f9b7571934093
ka0ru19 <k.prfm19@gmail.com>
2017-07-07 17:58:06 +0900

    最初の状態

--- cw/ViewController.swift
+++ cw/ViewController.swift
@@ -9,27 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    var n = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func plus() {
-        n += 1
-        label.text = String(n)
-    }
-    
-    @IBAction func clear() {
-        n = 0
-        label.text = String(n)
-    }
 }
-
