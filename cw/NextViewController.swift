3098fac1e57c7c167779b806815b9d3d3fa536b4
ka0ru19 <k.prfm19@gmail.com>
2017-07-07 18:44:11 +0900

    前の画面に戻る機能

--- cw/NextViewController.swift
+++ cw/NextViewController.swift
@@ -31,5 +31,7 @@ class NextViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
